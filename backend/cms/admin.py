85558fcaf3b5eaa5d4121cfefb56b1b2b6f296be
Florian Hofhammer <florian.hofhammer@fhofhammer.de>
2018-11-18 14:31:11 +0100

    Add basic event & POI creation functionality for admin backend
    This functionality is just for testing purposes in order to
    simply add events and POIs to the database

--- backend/cms/admin.py
+++ backend/cms/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 
-from .models import Site
+from .models.event import Event
+from .models.poi import POI
+from .models.site import Site
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'get_location_name')
+    list_filter = ['date', 'location__name']
+    search_fields = ['title', 'description', 'get_location_name']
+
+    def get_location_name(self, obj):
+        loc = obj.location
+        return 'Unknown' if loc is None else loc.name
+
+    get_location_name.short_description = 'Location'
+    get_location_name.admin_order_field = 'location__name'
+
+
+class POIAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'country')
+    list_filter = ['country', 'region', 'city']
+    search_fields = ['name', 'description', 'adress', 'postcode', 'city', 'country', 'region']
+
+
+admin.site.register(POI, POIAdmin)
+admin.site.register(Event, EventAdmin)
 
 admin.site.register(Site)

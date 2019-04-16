58faf8ded481003b7fa694be443a1192e1268946
Thomas Weizenegger <thomas.weiz97@gmail.com>
2019-04-16 13:29:24 +0200

    CMS's on admin view

--- backend/cms/admin.py
+++ backend/cms/admin.py
@@ -6,5 +6,7 @@ File routing to the admin site
 from django.contrib import admin
 
 from .models import Site
+from federated_cloud.models import CMSCache
 
 admin.site.register(Site)
+admin.site.register(CMSCache)

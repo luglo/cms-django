4a1ca66a41bb2144b298b0ec321510d928198ab1
Thomas Weizenegger <thomas.weiz97@gmail.com>
2019-07-15 13:54:04 +0200

    Add federated_cloud to migration script

--- dev-tools/migrate.sh
+++ dev-tools/migrate.sh
@@ -2,5 +2,6 @@
 
 source .venv/bin/activate
 integreat-cms makemigrations cms
+integreat-cms makemigrations federated_cloud
 integreat-cms migrate
 integreat-cms loaddata backend/cms/fixtures/roles.json

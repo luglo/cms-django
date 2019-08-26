5956d03ea526941f809a74a071fe8088d5170ca4
Thomas Weizenegger <thomas.weiz97@gmail.com>
2019-08-26 11:17:46 +0200

    test-stuff

--- backend/federated_cloud/tests.py
+++ backend/federated_cloud/tests.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 
 from federated_cloud.settings import *
-from federated_cloud.share_requests import send_offer
+from federated_cloud.share_requests import send_offer, ask_for_cms_data
+from federated_cloud.update_checker import update_cms_data
 
 
 def test(request):
@@ -17,3 +18,24 @@ def test(request):
 
     print(get_private_key())
     return HttpResponse("asdf")
+
+def test_activate(request):
+    name = "thomas"
+    domain = "thomas-ist-cool.de"
+    activate_federated_cloud_feature(name, domain)
+    return HttpResponse("FedCloud-Feature aktiviert.")
+
+def test_send_offer(request):
+    domain="localhost:8000"
+    send_offer(domain)
+    return HttpResponse("Angebot gesendet.")
+
+def test_update(reqeust):
+    update_cms_data()
+    return HttpResponse("Alles upgedatet.")
+
+def test_ask(request):
+    domain = "localhost:8000"
+    id = "bdd9bfbd9742d81a1680"
+    ask_for_cms_data(domain, id)
+    return HttpResponse("Anfrage gestellt.")
--- backend/federated_cloud/urls.py
+++ backend/federated_cloud/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     url(r'^cms-data/(?P<cms_id>[0-9,a-z]+)/$', sharing.cms_data),
     url(r'^region-data/', sharing.region_data),
     url(r'^offer/', sharing.receive_offer),
-    url(r'^test/', tests.test) #todo: remove test-stuff
+    url(r'^test/', tests.test), #todo: remove test-stuff
+    url(r'test-activate', tests.test_activate),
+    url(r'test-send-offer', tests.test_send_offer),
+    url(r'test-update', tests.test_update),
+    url(r'test-ask', tests.test_ask)
 ]
